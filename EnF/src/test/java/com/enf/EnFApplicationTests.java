0a35ab10bfdd8a9821104cafe61fa3b6c46b38d9
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-08 18:36:55 +0900

    first commit

---
+++ EnF/src/test/java/com/enf/EnFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnFApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
