b294efd9e4cf5119f9bee380a6b313c1f444caab
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-09 23:31:24 +0900

    feat : 예외처리 클래스 구현

---
+++ EnF/src/main/java/com/enf/exception/GlobalException.java
@@ -0,0 +1,19 @@
+package com.enf.exception;
+
+
+
+import com.enf.model.dto.response.ResultResponse;
+import com.enf.model.type.FailedResultType;
+import lombok.Getter;
+
+@Getter
+public class GlobalException extends RuntimeException {
+
+  private final ResultResponse resultResponse;
+
+  public GlobalException(FailedResultType failedResultCode) {
+    super("");
+    this.resultResponse = ResultResponse.of(failedResultCode);
+  }
+
+}
