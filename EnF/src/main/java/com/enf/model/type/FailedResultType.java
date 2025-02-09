5aadffa686c7299cd11bbb3a801b955c57bed123
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-09 23:12:13 +0900

    feat : Enum 클래스 생성

---
+++ EnF/src/main/java/com/enf/model/type/FailedResultType.java
@@ -0,0 +1,16 @@
+package com.enf.model.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum FailedResultType {
+  ACCESS_TOKEN_RETRIEVAL(HttpStatus.UNAUTHORIZED, "액세스 토큰을 가져오는 데 실패했습니다."),
+  USER_INFO_RETRIEVAL(HttpStatus.UNAUTHORIZED, "사용자 정보를 가져오는 데 실패했습니다."),
+  ;
+
+  private final HttpStatus status;
+  private final String message;
+}
---
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -0,0 +1,16 @@
+package com.enf.model.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessResultType {
+  SUCCESS_KAKAO_LOGIN(HttpStatus.OK, "로그인 성공"),
+  SUCCESS_KAKAO_SIGNUP(HttpStatus.OK, "회원가입 성공"),
+  ;
+
+  private final HttpStatus status;
+  private final String message;
+}
