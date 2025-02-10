0383c5a40c8d6ff0c60f6622735adb5c836b46d1
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-10 19:11:05 +0900

    feat : 사용하지 않은 import 구문 삭제

--- EnF/src/main/java/com/enf/controller/AuthController.java
+++ EnF/src/main/java/com/enf/controller/AuthController.java
@@ -2,11 +2,8 @@ package com.enf.controller;
 
 import com.enf.model.dto.response.ResultResponse;
 import com.enf.service.AuthService;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
