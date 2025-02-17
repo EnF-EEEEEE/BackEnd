8c00f39673a9c441499eec8bf9e33f1fabb09c99
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-18 00:23:54 +0900

    feat : 닉네임 중복 체크 성공시 상태코드 값 추가

--- EnF/src/main/java/com/enf/model/type/SuccessResultType.java
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessResultType {
+  SUCCESS_CHECK_NICKNAME(HttpStatus.OK, "닉네임 중복 체크 성공"),
   ;
 
   private final HttpStatus status;
