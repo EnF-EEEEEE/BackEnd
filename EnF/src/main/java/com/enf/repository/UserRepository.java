d5cacc51b7587e60d322747105fff21fd2cd1bd4
HobaeBang <bhb0407@naver.com>
2025-02-19 18:32:58 +0900

    feat : 주석 변경

--- EnF/src/main/java/com/enf/repository/UserRepository.java
+++ EnF/src/main/java/com/enf/repository/UserRepository.java
@@ -13,7 +13,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
   boolean existsByProviderId(String providerId);
 
   // providerId를 기준으로 lastLoginAt 업데이트하는 메서드
-  // TODO : 아직 querydsl 설정을 넣기 전이라 일단은 jpql을 사용하였음, 추후 변경 필요
+  // check : 아직 querydsl 설정을 넣기 전이라 일단은 jpql을 사용하였음, 추후 변경 필요
   @Modifying
   @Transactional
   @Query("UPDATE Users u SET u.lastLoginAt = CURRENT_TIMESTAMP WHERE u.providerId = :providerId")
