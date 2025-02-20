769889843c1adf8c1e5e1ae71bf2f4983abe06c3
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-21 01:53:56 +0900

    feat : 사용자 정보 조회를 위한 메서드 구현

--- EnF/src/main/java/com/enf/repository/UserRepository.java
+++ EnF/src/main/java/com/enf/repository/UserRepository.java
@@ -5,10 +5,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-  UserEntity findByEmail(String email);
 
   boolean existsByProviderId(String providerId);
 
