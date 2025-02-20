cf63dea4ac02e8c3cc56b60076d3ef81090b3a86
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-21 01:54:13 +0900

    feat : 사용자 추가 정보 등록 API 구현

--- EnF/src/main/java/com/enf/controller/UserController.java
+++ EnF/src/main/java/com/enf/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.enf.controller;
 
+import com.enf.model.dto.request.user.AdditionalInfoDTO;
 import com.enf.model.dto.response.ResultResponse;
 import com.enf.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +24,15 @@ public class UserController {
   public ResponseEntity<ResultResponse> checkNickname(@RequestParam("nickname") String nickname) {
 
     ResultResponse response = userService.checkNickname(nickname);
+
+    return new ResponseEntity<>(response, response.getStatus());
+  }
+
+  @PostMapping("/additional-info")
+  public ResponseEntity<ResultResponse> additionalInfo(
+      HttpServletRequest request, @RequestBody AdditionalInfoDTO additionalInfoDTO) {
+
+    ResultResponse response = userService.additionalInfo(request, additionalInfoDTO);
 
     return new ResponseEntity<>(response, response.getStatus());
   }
--- EnF/src/main/java/com/enf/service/impl/UserServiceImpl.java
+++ EnF/src/main/java/com/enf/service/impl/UserServiceImpl.java
@@ -1,9 +1,21 @@
 package com.enf.service.impl;
 
+import com.enf.entity.BirdEntity;
+import com.enf.entity.CategoryEntity;
+import com.enf.entity.RoleEntity;
+import com.enf.entity.UserEntity;
+import com.enf.exception.GlobalException;
+import com.enf.model.dto.request.user.AdditionalInfoDTO;
+import com.enf.model.dto.request.user.UserCategoryDTO;
 import com.enf.model.dto.response.ResultResponse;
+import com.enf.model.type.FailedResultType;
 import com.enf.model.type.SuccessResultType;
+import com.enf.repository.BirdRepository;
+import com.enf.repository.CategoryRepository;
+import com.enf.repository.RoleRepository;
 import com.enf.repository.UserRepository;
 import com.enf.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +26,9 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final BirdRepository birdRepository;
+  private final RoleRepository roleRepository;
+  private final CategoryRepository categoryRepository;
 
   /**
    * 회원가입 or 회원정보 수정시 닉네임 중복 검증을 위한 메서드
@@ -34,5 +49,26 @@ public class UserServiceImpl implements UserService {
 
     log.info("{} : 사용가능한 닉네임", nickname);
     return new ResultResponse(SuccessResultType.SUCCESS_CHECK_NICKNAME, false);
+  }
+
+  @Override
+  public ResultResponse additionalInfo(HttpServletRequest request,
+      AdditionalInfoDTO additionalInfoDTO) {
+
+    // 사용자 정보 추출, 추후 Jwt 토큰 로직 구현 후 수정할 예정
+    UserEntity user = userRepository.findById(1L)
+        .orElseThrow(() -> new GlobalException(FailedResultType.USER_NOT_FOUND));
+
+    BirdEntity bird = birdRepository.findByBirdName(additionalInfoDTO.getBirdName());
+
+    RoleEntity role = roleRepository.findByRoleName(additionalInfoDTO.getAgeGroup());
+
+    CategoryEntity category = role.getRoleName().equals("senior")
+        ? categoryRepository.save(UserCategoryDTO.of(additionalInfoDTO.getUserCategory()))
+        : null;
+
+    userRepository.save(AdditionalInfoDTO.of(user, bird, role, category, additionalInfoDTO));
+
+    return ResultResponse.of(SuccessResultType.SUCCESS_ADDITIONAL_USER_INFO);
   }
 }
