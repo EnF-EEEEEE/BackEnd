8f4f99dd3a83790cc6f60f4e67ff8bae147b933d
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-21 01:52:55 +0900

    feat : 사용자 추가 정보 등록을 위한 repository 구현

---
+++ EnF/src/main/java/com/enf/repository/BirdRepository.java
@@ -0,0 +1,12 @@
+package com.enf.repository;
+
+import com.enf.entity.BirdEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BirdRepository extends JpaRepository<BirdEntity, Long> {
+
+  BirdEntity findByBirdName(String birdName);
+
+}
