9feaa4a7b4aaa12a684ed4e487108aa04517e228
HobaeBang <bhb0407@naver.com>
2025-02-20 16:29:41 +0900

    refactor : path경로 이상 수정 & 테이블명 수정

--- EnF/src/main/java/com/enf/entity/UserEntity.java
+++ EnF/src/main/java/com/enf/entity/UserEntity.java
@@ -1,19 +1,15 @@
 package com.enf.entity;
 
 import com.enf.model.type.RoleType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.time.LocalDate;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "tb_user")
+import java.time.LocalDate;
+
+@Entity(name = "user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
