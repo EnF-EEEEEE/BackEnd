04d18fe01d7be38065924094356a30fd6a776922
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-21 01:51:54 +0900

    feat : 추가정보 등록 성공/실패 상태코드 값 추가

--- EnF/src/main/java/com/enf/model/type/SuccessResultType.java
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -10,6 +10,7 @@ public enum SuccessResultType {
   SUCCESS_KAKAO_LOGIN(HttpStatus.OK, "로그인 성공"),
   SUCCESS_KAKAO_SIGNUP(HttpStatus.OK, "회원가입 성공"),
   SUCCESS_CHECK_NICKNAME(HttpStatus.OK, "닉네임 중복 체크 성공"),
+  SUCCESS_ADDITIONAL_USER_INFO(HttpStatus.OK, "추가 정보 입력 성공"),
   ;
 
   private final HttpStatus status;
