530cf8f18293c79033b7f644a5f377adfbcc6245
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-22 21:05:22 +0900

    feat : 회원정보 수정 성공시 상태 코드값 추가

--- EnF/src/main/java/com/enf/model/type/SuccessResultType.java
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -11,6 +11,8 @@ public enum SuccessResultType {
   SUCCESS_KAKAO_SIGNUP(HttpStatus.OK, "회원가입 성공"),
   SUCCESS_CHECK_NICKNAME(HttpStatus.OK, "닉네임 중복 체크 성공"),
   SUCCESS_ADDITIONAL_USER_INFO(HttpStatus.OK, "추가 정보 입력 성공"),
+  SUCCESS_UPDATE_NICKNAME(HttpStatus.OK, "닉네임 수정 성공"),
+  SUCCESS_UPDATE_CATEGORY(HttpStatus.OK, "카테고리 수정 성공"),
   ;
 
   private final HttpStatus status;
