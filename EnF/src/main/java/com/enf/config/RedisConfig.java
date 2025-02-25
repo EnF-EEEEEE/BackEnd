30a57e325a87e0fc7c140dedd2ac7da1f077ed50
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-25 15:42:59 +0900

    feat : RedisConfig, QueryDslConfig 구현

---
+++ EnF/src/main/java/com/enf/config/RedisConfig.java
@@ -0,0 +1,102 @@
+package com.enf.config;
+
+import static java.time.Duration.ofMillis;
+
+import com.enf.service.impl.NotificationServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+  private final NotificationServiceImpl notificationService;
+
+  @Value("${spring.data.redis.host}")
+  private String host;
+
+  @Value("${spring.data.redis.port}")
+  private Integer port;
+
+  @Value("${spring.data.redis.timeout}")
+  private Long timeout;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+    config.setHostName(host);
+    config.setPort(port);
+
+    LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+        .commandTimeout(ofMillis(timeout))
+        .build();
+
+    return new LettuceConnectionFactory(config, clientConfig);
+  }
+
+  // 분산락을 사용하기 위한 RedisTemplate
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+    // Key 직렬화는 String으로
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+    // Value는 JSON으로 직렬화
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+    // Hash Key도 String으로
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+    // Hash Value는 JSON으로
+    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+    return redisTemplate;
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(RedisSerializationContext.SerializationPair
+            .fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair
+            .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+    return RedisCacheManager.builder(redisConnectionFactory)
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
+  }
+
+
+
+  @Bean
+  public MessageListenerAdapter messageListener() {
+    return new MessageListenerAdapter(notificationService);
+  }
+
+
+  //컨테이너 설정
+  @Bean
+  RedisMessageListenerContainer redisContainer() {
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(redisConnectionFactory());
+    container.addMessageListener(messageListener(), new ChannelTopic("notifications"));
+    return container;
+  }
+}
