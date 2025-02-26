366be94b7e2036612b1f63969407e2520dfe1e28
HobaeBang <bhb0407@naver.com>
2025-02-26 13:02:12 +0900

    feat : Optional 적용 및 findByUserSeq추가

--- EnF/src/main/java/com/enf/repository/UserRepository.java
+++ EnF/src/main/java/com/enf/repository/UserRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
   boolean existsByProviderId(String providerId);
+
+  Optional<UserEntity> findByProviderId(String providerId);
 
   // providerId를 기준으로 lastLoginAt 업데이트하는 메서드
   // check : 아직 querydsl 설정을 넣기 전이라 일단은 jpql을 사용하였음, 추후 변경 필요
@@ -21,4 +25,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
  
   boolean existsByNickname(String nickname);
 
+  Optional<UserEntity> findByUserSeq(Long userSeq);
 }
