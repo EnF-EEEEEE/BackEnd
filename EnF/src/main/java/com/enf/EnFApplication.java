fcb2d9865717992d4a5ff4bed7b15abf3f651145
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-02-27 02:12:20 +0900

    feat : 어노테이션 추가

--- EnF/src/main/java/com/enf/EnFApplication.java
+++ EnF/src/main/java/com/enf/EnFApplication.java
@@ -2,10 +2,12 @@ package com.enf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class EnFApplication {
 
   public static void main(String[] args) {
