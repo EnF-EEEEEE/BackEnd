8bf646579a58d9d96b281c9035ba625e5c5a7765
HobaeBang <bhb0407@naver.com>
2025-02-26 12:46:08 +0900

    feat : add loadUserById method

--- EnF/src/main/java/com/enf/service/AuthService.java
+++ EnF/src/main/java/com/enf/service/AuthService.java
@@ -2,9 +2,15 @@ package com.enf.service;
 
 
 import com.enf.model.dto.response.ResultResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface AuthService {
 
-  ResultResponse oAuthForKakao(String code);
+  ResultResponse oAuthForKakao(HttpServletResponse response, String code);
 
+  String loginTokenGenerator(ResultResponse resultResponse);
+
+  UserDetails loadUserById(Long userSeq) throws UsernameNotFoundException;
 }
