4acb93cc10d989247ecca9de15f43cb6f68fa20e
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-02 17:00:34 +0900

    feat : 요청 성공 응답 코드 추가

--- EnF/src/main/java/com/enf/model/type/SuccessResultType.java
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -19,6 +19,8 @@ public enum SuccessResultType {
   SUCCESS_RECEIVE_LETTER(HttpStatus.OK, "편지 답장 성공"),
   SUCCESS_GET_ALL_LETTER(HttpStatus.OK, "모든 편지 조회 성공"),
   SUCCESS_GET_PENDING_LETTER(HttpStatus.OK, "담장을 기다리는 편지 조회 성공"),
+  SUCCESS_GET_SAVE_LETTER(HttpStatus.OK, "저장한 편지 조회 성공"),
+  SUCCESS_SAVE_LETTER(HttpStatus.OK, "편지 저장 성공"),
   ;
 
   private final HttpStatus status;
