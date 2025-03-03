4f22bbe402baea2197276dd1c993c142ba7fe45c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-03 20:24:41 +0900

    feat : 편지 넘기기 Entity, Repository 구현

---
+++ EnF/src/main/java/com/enf/entity/ThrowLetterEntity.java
@@ -0,0 +1,32 @@
+package com.enf.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "throw_letter")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class ThrowLetterEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long throwLetterSeq;
+
+  @ManyToOne
+  @JoinColumn(name = "throw_user_seq")
+  private UserEntity throwUser;
+
+  @ManyToOne
+  @JoinColumn(name = "letter_status_seq")
+  private LetterStatusEntity letterStatus;
+}
