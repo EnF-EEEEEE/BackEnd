2ffc47b9715ae7a6d69ff37b7587074eadfd8ce6
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-04 04:25:04 +0900

    feat : 프론트 URL 타입 추가

--- EnF/src/main/java/com/enf/model/type/UrlType.java
+++ EnF/src/main/java/com/enf/model/type/UrlType.java
@@ -7,6 +7,7 @@ public enum UrlType {
 
   KAKAO_TOKEN_URL("https://kauth.kakao.com/oauth/token"),
   KAKAO_USER_INFO_URL("https://kapi.kakao.com/v2/user/me"),
+  FRONT_URL("추가 예정"),
   ;
 
   private final String url;
