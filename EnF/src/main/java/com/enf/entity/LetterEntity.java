df19c453fdf1ef87934ca32f708d63f7b439d429
HobaeBang <bhb0407@naver.com>
2025-03-06 19:47:11 +0900

    feat : 편지 Entity 항목 수정 & LetterStatusEntity 추가

--- EnF/src/main/java/com/enf/entity/LetterEntity.java
+++ EnF/src/main/java/com/enf/entity/LetterEntity.java
@@ -4,13 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity(name = "letter")
 @AllArgsConstructor
@@ -22,14 +21,6 @@ public class LetterEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long letterSeq;
-
-  @ManyToOne
-  @JoinColumn(name = "send_user_seq")
-  private UserEntity sendUser;
-
-  @ManyToOne
-  @JoinColumn(name = "receive_user_seq")
-  private UserEntity receiveUser;
 
   private String categoryName;
 
---
+++ EnF/src/main/java/com/enf/entity/LetterStatusEntity.java
@@ -0,0 +1,51 @@
+package com.enf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "letter_status")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class LetterStatusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long letterStatusSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "mentee_seq")
+    private UserEntity mentee;
+
+    @ManyToOne
+    @JoinColumn(name = "mentor_seq")
+    private UserEntity mentor;
+
+    @ManyToOne
+    @JoinColumn(name = "mentee_letter_seq")
+    private LetterEntity menteeLetter;
+
+    @ManyToOne
+    @JoinColumn(name = "mentor_letter_seq")
+    private LetterEntity mentorLetter;
+
+    @Column(name = "is_mentee_read")
+    private boolean isMenteeRead = false;  // 기본값 설정
+
+    @Column(name = "is_mentor_read")
+    private boolean isMentorRead = false;  // 기본값 설정
+
+    @Column(name = "is_mentee_saved")
+    private boolean isMenteeSaved = false;  // 기본값 설정
+
+    @Column(name = "is_mentor_saved")
+    private boolean isMentorSaved = false;  // 기본값 설정
+
+    private LocalDateTime createAt;
+}
