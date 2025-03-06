35c8c7212ea02fcf1c4c132e15b9ad57b84c63b6
HobaeBang <bhb0407@naver.com>
2025-03-06 19:53:05 +0900

    feat : 관라지 kakao 기능 추가

--- EnF/src/main/java/com/enf/component/KakaoAuthHandler.java
+++ EnF/src/main/java/com/enf/component/KakaoAuthHandler.java
@@ -5,20 +5,18 @@ import com.enf.exception.GlobalException;
 import com.enf.model.dto.request.auth.KakaoUserDetailsDTO;
 import com.enf.model.type.FailedResultType;
 import com.enf.model.type.UrlType;
-import java.util.Map;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -37,10 +35,12 @@ public class KakaoAuthHandler {
   @Value("${spring.security.oauth2.kakao.admin-key}")
   private String adminKey;
 
+  private String adminRedirectUri = "http://localhost:8080/api/v1/admin/callback";
+
   //토큰 조회를 위한 메서드
-  public String getAccessToken(String code) {
+  public String getAccessToken(HttpServletRequest request, String code) {
     log.info("KakaoAuthHandler.getAccessToken code={}", code);
-    HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = getMultiValueMapHttpEntity(code);
+    HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = getMultiValueMapHttpEntity(request,code);
 
     RestTemplate restTemplate = new RestTemplate();
     ResponseEntity<Map<String, Object>> response = restTemplate.exchange(
@@ -64,7 +64,8 @@ public class KakaoAuthHandler {
   }
 
   // 조회를 위한 request url 설정
-  private HttpEntity<MultiValueMap<String, String>> getMultiValueMapHttpEntity(String code) {
+  private HttpEntity<MultiValueMap<String, String>> getMultiValueMapHttpEntity(HttpServletRequest request, String code) {
+    log.info("requestURI={}", request.getRequestURI());
     HttpHeaders headers = new HttpHeaders();
     headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
@@ -73,7 +74,11 @@ public class KakaoAuthHandler {
     body.add("grant_type", "authorization_code");
     body.add("client_id", clientId);
     body.add("client_secret", clientSecretId);
-    body.add("redirect_uri", redirectUri);
+    if (request.getRequestURI().equals("/api/v1/admin/callback")) {
+      body.add("redirect_uri", adminRedirectUri);
+    } else {
+      body.add("redirect_uri", redirectUri);
+    }
     body.add("code", code);
 
     // HTTP 요청 보내기
