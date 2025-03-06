5caf49628c16f79ad79bf237319653abcac55180
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-06 15:29:01 +0900

    feat : 상태 코드값 수정

--- EnF/src/main/java/com/enf/model/type/SuccessResultType.java
+++ EnF/src/main/java/com/enf/model/type/SuccessResultType.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessResultType {
   SUCCESS_KAKAO_LOGIN(HttpStatus.OK, "로그인 성공"),
-  SUCCESS_KAKAO_SIGNUP(HttpStatus.OK, "회원가입 성공"),
+  SUCCESS_KAKAO_SIGNUP(HttpStatus.CREATED, "회원가입 성공"),
   SUCCESS_CHECK_NICKNAME(HttpStatus.OK, "닉네임 중복 체크 성공"),
   SUCCESS_ADDITIONAL_USER_INFO(HttpStatus.OK, "추가 정보 입력 성공"),
   SUCCESS_REISSUE_TOKEN(HttpStatus.OK, "토큰 재발급 성공"),
