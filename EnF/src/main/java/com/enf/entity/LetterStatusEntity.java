9adb8b2c634809421b3fa522fc6d16026c0b28a1
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-08 00:37:32 +0900

    feat : 간격 수정

--- EnF/src/main/java/com/enf/entity/LetterStatusEntity.java
+++ EnF/src/main/java/com/enf/entity/LetterStatusEntity.java
@@ -1,6 +1,5 @@
 package com.enf.entity;
 
-import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 @Entity(name = "letter_status")
 @AllArgsConstructor
@@ -46,31 +44,31 @@ public class LetterStatusEntity {
     @Column(name = "is_mentee_read")
     private boolean isMenteeRead = false;  // 기본값 설정
 
-  @Column(name = "is_mentor_read")
-  private boolean isMentorRead = false;  // 기본값 설정
+    @Column(name = "is_mentor_read")
+    private boolean isMentorRead = false;  // 기본값 설정
 
-  @Column(name = "is_mentee_saved")
-  private boolean isMenteeSaved = false;  // 기본값 설정
+    @Column(name = "is_mentee_saved")
+    private boolean isMenteeSaved = false;  // 기본값 설정
 
-  @Column(name = "is_mentor_saved")
-  private boolean isMentorSaved = false;  // 기본값 설정
+    @Column(name = "is_mentor_saved")
+    private boolean isMentorSaved = false;  // 기본값 설정
 
-  @Column(name = "is_thanks_to_mentor")
-  private boolean isThanksToMentor;
+    @Column(name = "is_thanks_to_mentor")
+    private boolean isThanksToMentor;
 
-  private LocalDateTime createAt;
+    private LocalDateTime createAt;
 
-  public static LetterStatusEntity of(LetterEntity menteeLetter, UserEntity mentee, UserEntity mentor) {
-    return LetterStatusEntity.builder()
-        .mentee(mentee)
-        .mentor(mentor)
-        .menteeLetter(menteeLetter)
-        .isMenteeRead(false)
-        .isMentorRead(false)
-        .isMenteeSaved(false)
-        .isMentorSaved(false)
-        .isThanksToMentor(false)
-        .createAt(LocalDateTime.now())
-        .build();
-  }
+    public static LetterStatusEntity of(LetterEntity menteeLetter, UserEntity mentee, UserEntity mentor) {
+      return LetterStatusEntity.builder()
+          .mentee(mentee)
+          .mentor(mentor)
+          .menteeLetter(menteeLetter)
+          .isMenteeRead(false)
+          .isMentorRead(false)
+          .isMenteeSaved(false)
+          .isMentorSaved(false)
+          .isThanksToMentor(false)
+          .createAt(LocalDateTime.now())
+          .build();
+    }
 }
