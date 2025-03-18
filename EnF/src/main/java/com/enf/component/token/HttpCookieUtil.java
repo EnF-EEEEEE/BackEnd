3faa017300c88dc18f49afe90e033a56f8276f92
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-03-19 03:00:35 +0900

    refactor : 쿠키 설정 변경

--- EnF/src/main/java/com/enf/component/token/HttpCookieUtil.java
+++ EnF/src/main/java/com/enf/component/token/HttpCookieUtil.java
@@ -6,10 +6,11 @@ import org.springframework.http.ResponseCookie;
 public class HttpCookieUtil {
     public static ResponseCookie addCookieToResponse(String refreshToken){
         return ResponseCookie.from(TokenType.REFRESH.getValue(), refreshToken)
-                .httpOnly(true)               //JavaScript에서 접근 불가능
-                .secure(true)                 //HTTP에서만 전송
-                .path("/")                    //전체 도메인에서 유효
-                .maxAge(7200)   //2시간 유효
-                .build();
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(2 * 60 * 60)
+            .sameSite("None")
+            .build();
     }
 }
