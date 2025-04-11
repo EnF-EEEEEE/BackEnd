05b41ea3846c5cd4408a59316385f38a6791e356
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-04-11 18:33:53 +0900

    feat : 공지사항 조회 및 상세 조회 API 구현

--- EnF/module-api/src/main/java/com/enf/api/controller/AnnouncementsController.java
+++ EnF/module-api/src/main/java/com/enf/api/controller/AnnouncementsController.java
@@ -1,12 +1,14 @@
 package com.enf.api.controller;
 
 import com.enf.api.service.AnnouncementsService;
-import com.enf.domain.model.dto.request.notification.AnnouncementsDTO;
+import com.enf.domain.model.dto.request.announcements.AnnouncementsDTO;
 import com.enf.domain.model.dto.response.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,21 @@ public class AnnouncementsController {
   public ResponseEntity<ResultResponse> createAnnouncements(AnnouncementsDTO announcementsDTO) {
 
     ResultResponse response = announcementsService.createAnnouncements(announcementsDTO);
+    return new ResponseEntity<>(response, response.getStatus());
+  }
+
+  @GetMapping("")
+  public ResponseEntity<ResultResponse> getAnnouncements() {
+
+    ResultResponse response = announcementsService.getAnnouncements();
+    return new ResponseEntity<>(response, response.getStatus());
+  }
+
+  @GetMapping("/details")
+  public ResponseEntity<ResultResponse> getAnnouncementsDetails(
+      @RequestParam(name = "announcementSeq") Long announcementSeq) {
+
+    ResultResponse response = announcementsService.getAnnouncementsDetails(announcementSeq);
     return new ResponseEntity<>(response, response.getStatus());
   }
 
--- EnF/module-api/src/main/java/com/enf/api/service/impl/AnnouncementsServiceImpl.java
+++ EnF/module-api/src/main/java/com/enf/api/service/impl/AnnouncementsServiceImpl.java
@@ -1,11 +1,19 @@
 package com.enf.api.service.impl;
 
+import com.enf.api.exception.GlobalException;
 import com.enf.api.service.AnnouncementsService;
 import com.enf.domain.entity.AnnouncementsEntity;
-import com.enf.domain.model.dto.request.notification.AnnouncementsDTO;
+import com.enf.domain.model.dto.request.announcements.AnnouncementsDTO;
 import com.enf.domain.model.dto.response.ResultResponse;
+import com.enf.domain.model.dto.response.announcements.Announcements;
+import com.enf.domain.model.dto.response.announcements.AnnouncementsDetails;
+import com.enf.domain.model.type.FailedResultType;
 import com.enf.domain.model.type.SuccessResultType;
 import com.enf.domain.repository.AnnouncementsRepository;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,4 +31,32 @@ public class AnnouncementsServiceImpl implements AnnouncementsService {
 
     return ResultResponse.of(SuccessResultType.SUCCESS_CREATE_ANNOUNCEMENT);
   }
+
+  @Override
+  public ResultResponse getAnnouncements() {
+    int currentDate = LocalDateTime.now().getDayOfWeek().getValue() - DayOfWeek.MONDAY.getValue();
+
+    // 이번 주 월요일 계산 (시작일)
+    LocalDateTime startOfWeek = LocalDate.now().minusDays(currentDate).atTime(0, 0, 0);
+
+    // 이번 주 일요일 계산 (종료일)
+    LocalDateTime endOfWeek = startOfWeek.plusDays(6).withHour(23).withMinute(59).withSecond(59);
+
+    List<Announcements> announcements = Announcements.of(announcementsRepository
+        .findByCreatedAtBetween(startOfWeek, endOfWeek));
+
+
+    return new ResultResponse(SuccessResultType.SUCCESS_GET_ANNOUNCEMENT, announcements);
+  }
+
+  @Override
+  public ResultResponse getAnnouncementsDetails(Long announcementSeq) {
+    AnnouncementsDetails announcementsDetails = AnnouncementsDetails.of(announcementsRepository
+        .findById(announcementSeq)
+        .orElseThrow(() -> new GlobalException(FailedResultType.ANNOUNCEMENT_NOT_FOUND)));
+
+    return new ResultResponse(SuccessResultType.SUCCESS_CREATE_ANNOUNCEMENT, announcementsDetails);
+  }
+
+
 }
