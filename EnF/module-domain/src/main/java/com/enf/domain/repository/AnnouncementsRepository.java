16f2af8b6b30fcd3ae70f907f592a290c95aeaf8
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-04-11 18:34:37 +0900

    feat : 이번주 공지사항을 불러오는 쿼리문 구현

--- EnF/module-domain/src/main/java/com/enf/domain/repository/AnnouncementsRepository.java
+++ EnF/module-domain/src/main/java/com/enf/domain/repository/AnnouncementsRepository.java
@@ -1,10 +1,13 @@
 package com.enf.domain.repository;
 
 import com.enf.domain.entity.AnnouncementsEntity;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnnouncementsRepository extends JpaRepository<AnnouncementsEntity, Long> {
 
+ List<AnnouncementsEntity> findByCreatedAtBetween(LocalDateTime startOfWeek, LocalDateTime endOfWeek);
 }
