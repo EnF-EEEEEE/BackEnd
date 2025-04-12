eebef0e4670004c67868a5424b6162fdee0e7eec
HobaeBang <bhb0407@naver.com>
2025-04-13 01:31:18 +0900

    DEAR-133 feat : add WebMvc Config

---
+++ EnF/module-email/src/main/java/com/enf/email/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.enf.email.config;
+
+import com.enf.email.interceptor.ApiKeyInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApiKeyInterceptor apiKeyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiKeyInterceptor)
+                .addPathPatterns("/api/email/**"); // 이메일 API 경로에만 인터셉터 적용
+    }
+}
