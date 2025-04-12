5a805d41a0029537665ffba1149b5ea0e029c3ee
HobaeBang <bhb0407@naver.com>
2025-04-13 01:36:09 +0900

    DEAR-133 feat : 각 케이스별 요청 Controller method 생성

--- EnF/module-email/src/main/java/com/enf/email/controller/EmailController.java
+++ EnF/module-email/src/main/java/com/enf/email/controller/EmailController.java
@@ -2,34 +2,55 @@ package com.enf.email.controller;
 
 import com.enf.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/email")
+@RequestMapping("/api/v1/email/send")
 @RequiredArgsConstructor
+@Slf4j
 public class EmailController {
     private final EmailService emailService;
 
-    @PostMapping("/send")
-    public ResponseEntity<String> sendEmail(
-            @RequestParam String to,
-            @RequestParam String subject,
-            @RequestParam String text) {
-
-        emailService.sendSimpleEmail(to, subject, text);
-        return ResponseEntity.ok("이메일이 성공적으로 전송되었습니다");
+    @PostMapping("/mentee-letter-arrived")
+    public ResponseEntity<String> sendMenteeLetterArrivedEmail(
+            @RequestParam String email,
+            @RequestParam String nickname) {
+        log.info("멘티 편지 도착 이메일 요청: {}", email);
+        emailService.sendMenteeLetterArrivedEmail(email, nickname);
+        return ResponseEntity.ok("멘토에게 편지 도착 알림 이메일이 성공적으로 전송되었습니다");
     }
 
-    @PostMapping("/send-html")
-    public ResponseEntity<String> sendHtmlEmail(
-            @RequestParam String to,
-            @RequestParam String subject) {
+    @PostMapping("/mentor-letter-arrived")
+    public ResponseEntity<String> sendMentorLetterArrivedEmail(
+            @RequestParam String email,
+            @RequestParam String nickname) {
 
-        emailService.sendHtmlEmail(to, subject);
-        return ResponseEntity.ok("HTML 이메일이 성공적으로 전송되었습니다");
+        log.info("멘토 편지 도착 이메일 요청: {}", email);
+        emailService.sendMentorLetterArrivedEmail(email, nickname);
+        return ResponseEntity.ok("멘티에게 답장 도착 알림 이메일을 전송합니다.");
+    }
+
+    @PostMapping("/one-day-notice")
+    public ResponseEntity<String> sendOneDayNoticeEmail(
+            @RequestParam String email,
+            @RequestParam String nickname) {
+        log.info("하루 전 알림 이메일 요청: {}", email);
+        emailService.sendOneDayNoticeEmail(email, nickname);
+        return ResponseEntity.ok("멘티에게 답장 도착 알림 이메일을 전송합니다.");
     }
 }
+
+/*
+    클라이언트측 요청 내용
+
+    @PostMapping("/api/v1/email/send/mentee-letter-arrived")
+    String sendMenteeLetterArrivedEmail(@RequestParam("to") String to);
+
+    @PostMapping("/api/v1/email/send/mentor-letter-arrived")
+    String sendMentorLetterArrivedEmail(@RequestParam("to") String to);
+
+    @PostMapping("/api/v1/email/send/one-day-notice")
+    String sendOneDayNoticeEmail(@RequestParam("to") String to);
+ */
